642f25b80a205c7b4c785fc23dce7b7f61eaab19
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-02 18:32:15 -0800

    drag functional

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
 $(()=>{
     console.log("linked")
     
 
-    
+
 
     $.ajax({
         url: "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian",
