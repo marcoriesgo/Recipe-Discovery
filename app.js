f21ee46fd82cfea8c7038b5be957ac6b458eeebe
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-04 14:33:07 -0800

    added js elements

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-//Drag and Drop functionality:
+const recipe = "";
+const calories = 0;
+const fat = 0;
+const protein = 0;
+
+
+$(()=>{
+    console.log("linked")
 
 
 
-// $(()=>{
-//     console.log("linked")
+
+    $.ajax({
+        url: "https://api.edamam.com/search?q=steak&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182",
+        type: "GET",
+        data: {
+          "$limit" : 4
+        },
+    }).done(function(data) {
+      alert("We found your dinner!");
+      console.log(data);
+    });
     
-
-
-
-//     $.ajax({
-//         url: "https://api.edamam.com/search?q=steak&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182",
-//         type: "GET",
-//         data: {
-//           "$limit" : 3
-//         }
-//     }).done(function(data) {
-//       alert("Retrieved " + data.length + " records from the dataset!");
-//       console.log(data);
-//     });
-    
-//   })
+  })
