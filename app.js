d6a623451777895e31dafe2f32d64caf300aaf5b
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-12 20:17:15 -0800

    new html and css

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 console.log("linked")
+// let urlString = 'https://api.edamam.com/search?q=steak&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182&calories=' + calorieRange
+
 $(()=>{
 
 
-  $.ajax({
-    url: "https://api.edamam.com/search?q=steak&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182",
-    type: "GET",
-}).done(function(data) {
-  alert("We found your dinner!");
-  console.log(data);
-});
+
+  $("form").on("submit", () => {
+    event.preventDefault();
+    const userInput = $('input[type="text"]').val();
+    $.ajax({
+      url:
+        "https://api.edamam.com/search?q=steak&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182&calories=" +
+        $(event.target).text(),
+    }).then(
+      data => {
+        console.log(data);
+        createList(data);
+      },
+      () => {
+        console.log("bad");
+      }
+    );
+  });
+
 
 
 })
