1474de884263dd33176c241f3fb3b2b95a6c4d77
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-12 18:08:03 -0800

    restart work, calories and food type

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-const app_id = "cd8e8580";
-const app_key = "f8d1fa2b821a6224e531e36c5b111182";
-
-
-
+console.log("linked")
 $(()=>{
-    console.log("linked")
 
 
-    function foodSearch(food){
-      ajax_get(`https://api.edamam.com/search?q=${food}&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182`,
-      function(data) {
-          if (data.length == 0) {
-            clearRecipe();
-            $("#recipe").append("<tr><td>There is no image for that recipe.</td></tr>");
-          } else {
-            displayRecipe(data[0]);
-          }
-      });
-  }
-
-  function clearRecipe() {
-    $('#recipeImage').attr('src', "");
-    $("#recipeTable tr").remove();
-  }
-
-  function displayRecipe(image) {
-    $('#recipeImage').attr('src', image.url);
-    $("#recipeTable tr").remove();
+  $.ajax({
+    url: "https://api.edamam.com/search?q=steak&app_id=cd8e8580&app_key=f8d1fa2b821a6224e531e36c5b111182",
+    type: "GET",
+}).done(function(data) {
+  alert("We found your dinner!");
+  console.log(data);
+});
 
 
-  })
+})
